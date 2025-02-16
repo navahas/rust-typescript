5f5adcf02db1e96a3b7bea4bb580d88f5944762c
cnavajas <c.navajas4@gmail.com>
2025-02-16 08:59:45 +0100

    misc: borrow checker 2

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,15 @@ struct Item{
     count: usize
 }
 
-fn add_one(item: &mut Item) {
-    item.count += 1;
+fn print_all(items: &Vec<Item>) {
+    for item in items {
+        println!("{:?}", item);
+    }
 }
-
 fn main() {
-    let mut item = Item { count: 1 };
-    add_one(&mut item);
-    println!("{:?}", item)
+    let mut items = vec![Item { count: 1 }];
+    let first = items.first_mut();
+    println!("{:?}", first);
+    
+    print_all(&items);
 }
