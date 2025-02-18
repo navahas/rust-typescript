97173bb53ed4a1b056f686d255bf7349a47967ff
Carlos Navajas <c.navajas4@gmail.com>
2025-02-18 08:37:13 +0100

    feat: IntoIterator

--- src/shapes/rect.rs
+++ src/shapes/rect.rs
@@ -31,3 +31,41 @@ impl Display for Rectangle {
         return write!(f, "Rectangle({}, {}): {}x{}", self.x, self.y, self.height, self.width);
     }
 }
+
+pub struct RectIter {
+    points: Vec<(f64, f64)>,
+    idx: usize
+}
+
+impl Iterator for RectIter {
+    type Item = (f64, f64);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.idx >= self.points.len() {
+            return None;
+        }
+
+        let point = self.points[self.idx];
+        self.idx += 1;
+
+        return Some(point);
+    }
+}
+
+impl IntoIterator for Rectangle {
+    type Item = (f64, f64);
+
+    type IntoIter = RectIter;
+
+    fn into_iter(self) -> Self::IntoIter {
+        return RectIter {
+            points: vec![
+                (self.x, self.y),
+                (self.x + self.width, self.y),
+                (self.x, self.y + self.height),
+                (self.x + self.width, self.y + self.height)
+            ],
+            idx: 0,
+        }
+    }
+}
